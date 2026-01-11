9f5627d82179a355920abec2b6303a539832ad20
simnem <150941742+simnem@users.noreply.github.com>
2026-01-11 22:32:51 +0100

    Implement CORS middleware for API
    Added CORS support to allow cross-origin requests.

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.static('public'));
+
+// === CORS ===
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // === CONFIG ===
 require('dotenv').config();
 
@@ -74,3 +89,4 @@ app.post('/generateToken', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
